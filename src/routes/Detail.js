9ed5f3ec14c1ca12c554b42b38f0ae10435a48b4
castle.jaemin <ehdrud1332@gmail.com>
2021-03-10 12:17:38 +0900

    2.2 Get Movie Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+//query에 var(변수)가 있을때 (ex: id??)는 적어야한다.
+// apollo, React, GraphQL, React Apollo를 위해.
+// 첫째 줄 query는 apollo를 위한 것
+// movie~ 부터는 우리 서버를 위한 것
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      rating
+      language
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
 
 const Detail = () => {
-    return (
-        <div>
-            Detail
-        </div>
-    );
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,31 +1,87 @@
-import React from 'react';
-import {gql, useQuery} from '@apollo/client';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
+// query에 var(변수)가 하나도 없을때는 특별하게 무엇을 적을 필요가 없다.
 const GET_MOVIES = gql`
-    {
-        movies{
-            id
-            medium_cover_image
-        }
+  {
+    movies {
+      id
+      medium_cover_image
     }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
 `;
 
 const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  // if(loading){
+  //     return "loading..."
+  // }
+  // if(data && data.movies) {
+  //     return data.movies.map(m => <h1>{m.id}</h1>)
+  // }
 
-    const {loading, data} = useQuery(GET_MOVIES);
-    if(loading){
-        return "loading..."
-    }
-    if(data && data.movies) {
-        return data.movies.map(m => <h1>{m.id}</h1>)
-    }
-
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo Movie</Title>
+        <Subtitle>With GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading ...</Loading>}
+      {!loading && data.movies && (
+        <Movies>
+          {data.movies.map((m) => (
+            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          ))}
+        </Movies>
+      )}
+    </Container>
+  );
 };
 
 export default Home;
-
